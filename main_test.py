f234e1a4997ff8028befbf357549b9e93e9cf40c
Cipher-implosion <bua80vo@gmail.com>
2025-03-10 17:28:41 +0900

    メインのテストファイル作成（初期設定無し）

---
+++ main_test.py
@@ -0,0 +1,23 @@
+from game_settings import get_blinds, get_num_players, get_players, assign_positions
+from preflop import preflop_action
+
+def main():
+    # small_blind, big_blind = get_blinds()
+    # num_players = get_num_players()  # ここで人数を取得
+    # players = get_players(num_players)  # num_players を渡す
+    # player_positions = assign_positions(players)
+
+    # print("スモールブラインド:", small_blind)
+    # print("ビッグブラインド:", big_blind)
+    # print("プレイヤー情報:", players)
+    # print("ポジション情報:", player_positions)
+    
+     # プリフロップの処理を実行
+    preflop_bets = preflop_action([{'name': 'soma', 'stack': 20000}, {'name': 'cipher', 'stack': 20000}], 
+                                  {'soma': 'SB', 'cipher': 'BB'}, 100, 300)
+    print("プリフロップのベット情報:", preflop_bets)
+
+
+
+if __name__ == "__main__":
+    main()
