40fee423818f268048839c563d07d79dad464dc0
Cipher-implosion <bua80vo@gmail.com>
2025-03-12 19:11:08 +0900

    DB接続の設定ファイルを追加

--- db_config.py
+++ db_config.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 # 接続情報を変数で定義
 # username = "cipher"   # mac
@@ -14,9 +15,5 @@ DB_URL = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
 # SQLAlchemy エンジンを作成
 engine = create_engine(DB_URL, echo=True)  # `echo=True` でSQLログを表示
 
-# 接続確認
-try:
-    with engine.connect() as conn:
-        print("データベースに接続成功！")
-except Exception as e:
-    print("データベース接続に失敗:", e)
+# セッションメーカーを作成
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
