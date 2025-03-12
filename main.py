51def1111ae73adf3d64cbce9f090b9fed4c38ae
Cipher-implosion <bua80vo@gmail.com>
2025-03-12 19:22:23 +0900

    mainのDB接続処理を追加

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from db_config import engine, SessionLocal
+from sqlalchemy import text
+
 from game_settings import get_blinds, get_num_players, get_players, assign_positions
 from preflop import preflop_action
 
+# データベース接続を確認
+def check_connection():
+    try:
+        with engine.connect() as conn:
+            print("データベースに接続成功！")
+    except Exception as e:
+        print("データベース接続に失敗:", e)
+
 def main():
+    # データベース接続確認
+    check_connection()
+
+    # ブラインドの取得s
     small_blind, big_blind = get_blinds()
     num_players = get_num_players()  # ここで人数を取得
     players = get_players(num_players)  # num_players を渡す
